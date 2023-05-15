595d42e40f18ec74389f282fb0598c3b09f376dd
Zosia Samosia <Zosia@Samosia.pl>
2023-05-15 13:51:23 +0200

    version2

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
 'use strict';
 
-const getData = () => {
-    fetch('https://api.nbp.pl/api/exchangerates/tables/a/?format=JSON')
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
-        console.log(result[0].rates);
-        result[0].rates.forEach((element, i) => {
-            const tr = document.createElement('tr');
-            tr.innerHTML = 
+const baseUrl = 'https://api.nbp.pl/api/exchangerates/tables/';
+
+const getCurrency = async (table) => {
+
+    try {
+
+        const response = await fetch(`${baseUrl}${table}`);
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+
+        console.log(`To jest błąd: ${error}`);
+
+    }
+
+}
+
+getCurrency('b').then(data => {
+
+    console.log(data);
+
+    // destrukturyzacja
+    // const currencies = data[0]; to to samo co niżej
+    const [currencies] = data;
+
+    const { rates } = currencies;
+
+    rates.forEach((element, i) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML =
             `
             <td>${++i}</td>
             <td>${element.currency}</td>
             <td>${element.mid}</td>
             `
-            document.querySelector('table tbody').appendChild(tr);
-        });
-
-    })
-}   
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', getData);
+        document.querySelector('table tbody').appendChild(tr);
+    });
+});
